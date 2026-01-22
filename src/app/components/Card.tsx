73246ed34fdfc3d115da4524c8e838f95ef79714
Gurjinder7 <dev.gurjinder@gmail.com>
2026-01-22 17:50:42 +0000

    zustand store usage

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,13 +1,31 @@
+"use client"
+
 import {IProduct} from "@/utils/interface/product";
+// import {useStore} from "zustand/react";
+import useAppStore from "../../../store";
+import {useEffect} from "react";
 
 export const ProductCard = ({product}:{product:IProduct}) => {
     const {name, id, genre, price, year, thumbnail} = product;
+
+    const {products, addProduct } = useAppStore()
+
+    const addToCart = () => {
+        console.log("addToCart")
+        addProduct(product)
+    }
+
+    useEffect(() => {
+        console.log(products)
+    })
+
+    console.log(products)
     return (
         <div className="flex flex-col p-4 min-w-2xs max-w-3xl m-3 bg-white">
             <img src={thumbnail} className="" alt={name}/>
             <h3 className="text-2xl text-center text-black">{name}</h3>
             <p className="text-2xl font-bold text-center text-black">${price}</p>
-            <button className="bg-amber-700 text-white py-2 hover:bg-amber-600 hover:cursor-pointer">Add to Cart</button>
+            <button className="bg-amber-700 text-white py-2 hover:bg-amber-600 hover:cursor-pointer" onClick={addToCart}>Add to Cart</button>
         </div>
     )
 }
--- store.ts
+++ store.ts
@@ -2,19 +2,21 @@ import { create } from "zustand"
 import {IProduct} from "@/utils/interface/product";
 import {IState} from "@/utils/interface/State";
 
-const AppState = create((set) => ({
+const useAppStore = create((set) => ({
     products:[],
     authenticated: false,
     setAuthenticated: (status: boolean) => set((state:IState) => ({authenticated: status})),
-    addProduct: (product: IProduct) => ({
-        set(state : IState) {
-            state.products.push(product)
-        }
-    }),
+    addProduct: (product: IProduct) => {
+        console.log(product);
+        set((state:IState) => ({products: [...state.products, product]}));
+
+    },
     removeProduct: (product: IProduct) => ({
         set(state:IState) {
             state.products = state.products.filter(product => product.id !== product.id)
         }
     })
 
-}))
+}));
+
+export default useAppStore;
