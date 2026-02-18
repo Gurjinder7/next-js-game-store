7e6ff5a4ce665a449d582a4c2d5617ad515807d2
Gurjinder7 <dev.gurjinder@gmail.com>
2026-02-18 13:57:43 +0000

    supabase in playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -58,9 +58,15 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
+  webServer: [
+      {
     command: 'npm run dev',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
   },
+      {
+      command: 'npm run dev',
+          url: 'http://127.0.0.1:54321',
+      }
+  ],
 });
