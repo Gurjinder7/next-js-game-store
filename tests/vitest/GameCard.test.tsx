6a471eac1214e331db607123a004edaae205805d
Gurjinder7 <dev.gurjinder@gmail.com>
2026-02-19 14:11:40 +0000

    Game card snapshot test

--- tests/vitest/GameCard.test.tsx
+++ tests/vitest/GameCard.test.tsx
@@ -1,17 +1,23 @@
-import {expect, test} from "vitest";
+import { expect, test } from "vitest";
 import { ProductCard } from "../../src/app/components/Card.tsx";
-import {render, screen} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
+const product = {
+  name: "test",
+  id: 1,
+  genre: "Action",
+  price: 1.2,
+  year: 2001,
+  thumbnail: "some_url",
+  created_at: "now",
+};
 
-test('Product Card Rendering', async () => {
-    render(<ProductCard product={{
-        name:'test',
-        id:1,
-        genre:'Action',
-        price: 1.2,
-        year: 2001,
-        thumbnail: 'some_url',created_at: 'now'
-    }}/>)
-    await expect(screen.getByText('See details')).toBeVisible()
+test("Product Card Rendering", async () => {
+  render(<ProductCard product={product} />);
+  expect(screen.getByText("See details")).toBeVisible();
+});
 
-})
+test("GameCard component snapshot", () => {
+  const asFragment = render(<ProductCard product={product} />);
+  expect(asFragment).toMatchSnapshot();
+});
