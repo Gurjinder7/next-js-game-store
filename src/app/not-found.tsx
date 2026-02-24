23d515749bf3edfdbcbf7573cc60c3326de764f9
Gurjinder7 <dev.gurjinder@gmail.com>
2026-02-24 22:37:20 +0000

    not found file added

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+
+export default function NotFound() {
+    return <div className="flex flex-col items-center justify-center">
+        <Image src="/404.svg" width={500} height={500} alt="404 image" />
+        <p className="text-2xl text-violet-500 font-semibold">Page not found! </p>
+    </div>
+}
